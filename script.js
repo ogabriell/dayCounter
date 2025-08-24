ca8ebd497738a4b5c11f4ffae306b2d5f40fca25
ogabriell1 <srgabriel1305@gmail.com>
2025-08-24 20:01:58 -0300

    v1

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("funcionando")
+const getDate = () => {
+    oDateOne = document.getElementById("dateOne")
+    oDateTwo = document.getElementById("dateTwo")
+    console.log(new Date(oDateOne.value))
+//    console.log((new Date(oDateTwo.value) - new Date(oDateOne.value)) / (1000 * 60 * 60 * 24))
+}
